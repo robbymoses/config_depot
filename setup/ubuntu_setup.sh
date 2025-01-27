87167d5ec15eaefbaf7ab19ae430dc19cd648b58
Robert Moses <rmoses@titaniumbytes.com>
2025-01-27 13:35:16 -0600

    Adding SSH Keygen

--- setup/ubuntu_setup.sh
+++ setup/ubuntu_setup.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
 ## Packages File
-packages=~/config_depot/setup/packages.list
+PACKAGE_LIST=$HOME/config_depot/setup/packages.list
 
 # Custom Install Scripts
-custom_installers=~/config_depot/setup/custom_installers/*
+CUSTOM_INSTALLERS=$HOME/config_depot/setup/custom_installers/*
+
+# Public SSH Key
+PUBLIC_KEY=$HOME/.ssh/id_ed25519.pub
 
 ## Update && Upgrade
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 
 ## Install Script to Set up packages for WSL.
-sudo apt-get install -y $(grep -vE "^\s*#" $packages | tr "\n" "\t")
+sudo apt-get install -y $(grep -vE "^\s*#" $PACKAGE_LIST | tr "\n" "\t")
 
 ## Install Additional Scripts
-for file in $custom_installers
+for file in $CUSTOM_INSTALLERS
 do
    sh $file
 done
 
 ## Update shell
 sudo chsh -s /bin/zsh $USER
+
+## Stow Config Files
+stow -d ~/config_depot -t ~/
+
+## Generate SSH Key
+if [ -f $PUBLIC_KEY ]; then
+   echo "Found .ssh Key"
+   cat $PUBLIC_KEY
+else
+   echo "No Key Found... Generating."
+   cat $PUBLIC_KEY
+   #ssh-keygen -t ed25519 -C "rmoses@titaniumbytes.com"
+fi
+
+## Update Github Remote for Config
+git remote set-url origin git@github.com:robbymoses/config_depot.git
